fa60f7ba3e6443d504c4a3f214cc79bfffa02fe4
rh-el <williampp@hotmail.fr>
2026-09-19 00:58:21 +0200

    indexcss + tokens

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./pages/Home";
+import "../index.css";
 
 const router = createBrowserRouter([
 	{
