7b03faa1c749f8ec5f31550c3291eb749e4f1ee9
rh-el <williampp@hotmail.fr>
2026-09-20 15:25:15 +0200

    eslint config

---
+++ server/eslint.config.js
@@ -0,0 +1,7 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config({ ignores: ["dist"] }, js.configs.recommended, ...tseslint.configs.recommended, {
+	languageOptions: { globals: globals.node },
+});
